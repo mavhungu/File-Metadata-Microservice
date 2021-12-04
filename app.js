4937255c31c7f00a2b92a495350820e91bebbe77
ronewa <ronewamavhungu@gmail.com>
2021-12-04 08:08:53 +0200

    adding project mvc

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const multer = require('multer')
+const hbs = require('hbs')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'templates/views'))
+hbs.registerPartials(path.join(__dirname,'templates/partials'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
